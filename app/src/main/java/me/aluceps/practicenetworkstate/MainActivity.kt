28f545ffcd9a71b3b7db6470f644d17e32512a1c
mery-takano <atsushi.takano@mery.co.jp>
2021-03-11 20:39:56 +0900

    リファクタリング

--- app/src/main/java/me/aluceps/practicenetworkstate/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicenetworkstate/MainActivity.kt
@@ -2,7 +2,10 @@ package me.aluceps.practicenetworkstate
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
@@ -21,21 +24,62 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        Snackbar.make(binding.root, "Network is online? : %b".format(isOnline(this)), Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(binding.root, createNetworkStateMessage(this), Snackbar.LENGTH_SHORT).show()
     }
+
+    private fun createNetworkStateMessage(context: Context): String =
+            MESSAGE_FORMAT.format(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getNetworkState(context)
+            } else {
+                getConnectionState(context)
+            })
 
     /**
      * ネットワークの接続状態を取得
      * @see https://developer.android.com/training/basics/network-ops/managing?hl=ja
      */
-    private fun isOnline(context: Context): Boolean {
-        val connectiveManager = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            context.getSystemService(ConnectivityManager::class.java)
-        } else {
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        }
+    private fun getConnectionState(context: Context): ConnectionState =
+            (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).let { manager ->
+                manager.activeNetworkInfo.let { networkInfo ->
+                    when (networkInfo != null && networkInfo.isConnected) {
+                        true -> ConnectionState.ONLINE
+                        else -> ConnectionState.OFFLINE
+                    }
+                }
+            }
 
-        val networkInfo = connectiveManager.activeNetworkInfo
-        return networkInfo != null && networkInfo.isConnected
+    /**
+     * ネットワークの状態を取得
+     */
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun getNetworkState(context: Context): NetworkState =
+            context.getSystemService(ConnectivityManager::class.java).let { manager ->
+                manager.getNetworkCapabilities(manager.activeNetwork).let { capabilities ->
+                    if (capabilities != null) {
+                        when {
+                            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> NetworkState.WIFI
+                            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> NetworkState.MOBILE
+                            else -> NetworkState.OTHER
+                        }
+                    } else {
+                        NetworkState.NOT_CONNECTED
+                    }
+                }
+            }
+
+    enum class ConnectionState {
+        ONLINE,
+        OFFLINE
+    }
+
+    enum class NetworkState {
+        WIFI,
+        MOBILE,
+        OTHER,
+        NOT_CONNECTED
+    }
+
+    companion object {
+        const val MESSAGE_FORMAT = "Network is: %s"
     }
 }
